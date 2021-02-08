1dddc95ce312ed3a947040f1c00a820fa83d8f3a
Sabrina <sa_santillan@hotmail.com>
2021-02-08 16:17:35 -0300

    Se agrega ejercicio 1

---
+++ src/com/programacion1/tp01/dominio/MiPrimeraClase.java
@@ -0,0 +1,11 @@
+package com.programacion1.tp01.dominio;
+
+public class MiPrimeraClase {
+
+	public static void main(String[] args) {
+		
+		System.out.println("HAHAHA Tenés que empezar de nuevo LALALALA");
+
+	} // end main
+
+} // end MiPrimeraClase class
